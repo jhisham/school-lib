8b7874c66b4572cb01b7d55160ff1ca9cc8101ac
jhisham <jafry.hisham@gmail.com>
2022-07-28 00:38:47 +0800

    modify postgres db settings

--- school_library/settings.py
+++ school_library/settings.py
@@ -113,6 +113,11 @@ elif ENVIRONMENT == 'production':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env.str('DATABASE_NAME'),
+            'USER': env.str('DATABASE_USER'),
+            'PASSWORD': env.str('DATABASE_PASSWORD'),
+            'HOST': env.str('DATABASE_HOST'),
+            'PORT': env.str('DATABASE_PORT'),
             'DATABASE_URL': env.str('DATABASE_URL'),
         },
     }
